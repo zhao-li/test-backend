6b6089f96ad28e84e423226347aaedc855e10905
Zhao Li <zhao.li@startingspark.com>
2020-10-18 14:30:46 -1000

    integrate database with django application

--- porter/settings.py
+++ porter/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'porter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': os.environ['DATABASE_PORT'],
     }
 }
 
--- scripts/setup_app.sh
+++ scripts/setup_app.sh
@@ -3,6 +3,9 @@
 # This script sets up the application
 
 SRC_DIR=$(cd "$(dirname "$0")" || exit; pwd -P)
+. "$SRC_DIR"/setup_app/functions.sh
+
+wait_for_service "$DATABASE_HOST" "$DATABASE_PORT"
 
 python manage.py migrate
 "$SRC_DIR"/setup_app/add_admin_account.sh
