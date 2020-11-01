6ae2ccc5ee768c45d794a95b499c8161891091a3
Zhao Li <zhao.li@startingspark.com>
2020-10-31 16:44:07 -1000

    not upgrading pip seems to not reset env vars

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -32,7 +32,7 @@ gather_options() {
 set_defaults
 gather_options "$@"
 
-pip install --upgrade pip
+#pip install --upgrade pip
 case $environment in
   'production')
     pip install -r requirements.txt
