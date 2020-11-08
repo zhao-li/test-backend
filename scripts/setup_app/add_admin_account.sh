a9a565869399b63f4bfa5d2aa4ebb8ad2ede61da
Zhao Li <zhao.li@startingspark.com>
2020-11-07 20:01:08 -1000

    switch from sql command to django-admin command for creating admin user

--- scripts/setup_app/add_admin_account.sh
+++ scripts/setup_app/add_admin_account.sh
@@ -2,5 +2,8 @@
 
 # This script add an admin user to the Django admin app
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@ad.min', 'admin')" | python manage.py shell
+DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_EMAIL=admin@admin.admin \
+DJANGO_SUPERUSER_PASSWORD=admin \
+python manage.py createsuperuser --noinput
 
