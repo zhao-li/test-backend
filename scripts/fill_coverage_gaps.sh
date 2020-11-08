673479acd008ded1793b2484b8891ad37c01faf2
Zhao Li <zhao.li@startingspark.com>
2020-11-07 18:39:22 -1000

    switch default to allow coverage to pass and just enforce 100% coverage when necessary in ci/cd pipeline

--- scripts/fill_coverage_gaps.sh
+++ scripts/fill_coverage_gaps.sh
@@ -3,7 +3,8 @@
 # This script runs through the edge-case scenarios not handled by the normal
 # test suite to get coverage to 100%
 
-rm -rf /opt/app-root/lib/python3.8/site-packages/django/
-coverage run -a ./manage.py check
-coverage report --fail-under=0 # override the configured 100% coverage requirement to support CI/CD pipeline and development
+pip uninstall --yes django
+coverage run --append ./manage.py check
+coverage report --fail-under=1000
+pip install --requirement requirements.txt
 
