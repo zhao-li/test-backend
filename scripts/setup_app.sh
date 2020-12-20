c71cf78952223f1d41410a4480ece3f3ab06c9bf
Zhao Li <zhao.li@startingspark.com>
2020-12-19 14:04:59 -1000

    reorganize function file to be shared across scripts

--- scripts/setup_app.sh
+++ scripts/setup_app.sh
@@ -3,7 +3,7 @@
 # This script sets up the application
 
 SRC_DIR=$(cd "$(dirname "$0")" || exit; pwd -P)
-. "$SRC_DIR"/setup_app/functions.sh
+. "$SRC_DIR"/common/functions.sh
 
 wait_for_service "$DATABASE_HOST" "$DATABASE_PORT"
 
