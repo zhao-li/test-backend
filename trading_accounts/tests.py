acf730dff66a9a20c71180d32ce9b3ea8350029a
Zhao Li <zhao.li@startingspark.com>
2021-01-17 11:07:23 -1000

    add initial test for trading accounts resource

--- trading_accounts/tests.py
+++ trading_accounts/tests.py
@@ -1,3 +1,17 @@
 """Definte tests for entire module"""
+from django.test import Client, TestCase, tag
+from rest_framework import status
 
-# Create your tests here.
+
+class TradingAccountTests(TestCase):
+    """Test Trading Account API"""
+
+    def setUp(self):
+        self.client = Client()
+
+    @tag('integration')
+    def test_fetching_list(self):
+        """test getting a list of trading accounts"""
+        response = self.client.get('/trading-accounts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
