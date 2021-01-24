3054a469c48baac0e7c417f61d2272e4fd6bcc55
Zhao Li <zhao.li@startingspark.com>
2021-01-24 12:53:30 -1000

    fix up reading tests

--- trading_accounts/tests/basic_trading_accounts/test_reading.py
+++ trading_accounts/tests/basic_trading_accounts/test_reading.py
@@ -1,22 +1,14 @@
-"""Define tests for entire module"""
-import json
+"""Define tests for reading basic"""
 from django.test import Client, TestCase, tag
 from rest_framework import status
-from ...models import TradingAccount
-from ..constants  import TYPE, PATH, CONTENT_TYPE
+from ..constants  import PATH
 
 
-class TradingAccountsTests(TestCase):
-    """Test Trading Account API"""
+class ReadingTests(TestCase):
+    """Test Reading"""
 
     def setUp(self):
         self.client = Client()
-
-    @tag('integration')
-    def test_fetching(self):
-        """test fetchingt"""
-        response = self.client.get(PATH)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     @tag('integration')
     def test_for_json_api_compliance(self):
@@ -25,64 +17,15 @@ class TradingAccountsTests(TestCase):
 
         expected_key = 'data'
         self.assertTrue(expected_key in response.json())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     @tag('integration')
-    def test_creating(self):
-        """test creating"""
-
-        name = 'A Trading Account Name'
-        json_data = {
-            'data': {
-                'type': TYPE,
-                'attributes': {
-                    'name': name
-                }
-            }
-        }
-
-        response = self.client.post(
-            PATH,
-            json.dumps(json_data),
-            content_type=CONTENT_TYPE,
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        expected_name = name
-        self.assertEqual(
-            TradingAccount.objects.first().name,
-            expected_name
-        )
-
-    @tag('integration')
-    def test_creating_and_getting(self):
-        """test creating and getting"""
-
-        name = 'A Trading Account Name'
-        json_data = {
-            'data': {
-                'type': TYPE,
-                'attributes': {
-                    'name': name
-                }
-            }
-        }
-
-        response = self.client.post(
-            PATH,
-            json.dumps(json_data),
-            content_type=CONTENT_TYPE,
-        )
-
-        expected_name = name
-        self.assertEqual(TradingAccount.objects.first().name, expected_name)
-
+    def test_reading(self):
+        """test reading"""
         response = self.client.get(PATH)
-        first_trading_account_index = 0
+        no_trading_accounts= 0
         self.assertEqual(
-            (response.json()
-                ['data'][first_trading_account_index]
-                ['attributes']['name']
-             ),
-            expected_name
+            len(response.json()['data']),
+            no_trading_accounts
         )
 
