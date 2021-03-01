6a45189cd6f8896eb88901913daae2d0adca86bb
Zhao Li <zhao.li@startingspark.com>
2021-02-28 15:51:54 -1000

    link up transaction view, urls, serializers, and models to get transaction creating to work

--- transactions/models.py
+++ transactions/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class Transaction(models.Model):
     """Define Transaction model"""
-    raw = models.JSONField()
+    raw = models.JSONField(null=True, blank=True)
 
--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+"""Defines views"""
+from rest_framework import viewsets
+from .models import Transaction
+from .serializers import TransactionSerializer
 
-# Create your views here.
+
+# pylint:disable=too-many-ancestors
+class TransactionViewSet(viewsets.ModelViewSet):
+    """API endpoints for Transaction"""
+
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
