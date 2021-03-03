c37a3f783a88212179bf2d2207dc4b53368f1374
Zhao Li <zhao.li@startingspark.com>
2021-03-02 19:33:02 -1000

    update model and migration to support relationship between transaction and trading account

--- transactions/models.py
+++ transactions/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class Transaction(models.Model):
     """Define Transaction model"""
     raw = models.JSONField(null=True, blank=True)
+    account = models.ForeignKey('trading_accounts.TradingAccount', on_delete=models.CASCADE)
 
