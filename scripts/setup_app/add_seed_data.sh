3875085366c155ebb953ad8e40d087efa514e102
Zhao Li <zhao.li@startingspark.com>
2021-03-13 18:56:54 -1000

    combine seeding commands

--- scripts/setup_app/add_seed_data.sh
+++ scripts/setup_app/add_seed_data.sh
@@ -2,7 +2,8 @@
 
 # This script adds seed data
 
-./manage.py loaddata greetings/seeds.yaml
-./manage.py loaddata trading_accounts/seeds.yaml
-./manage.py loaddata transactions/seeds.yaml
+./manage.py loaddata \
+  greetings/seeds.yaml \
+  trading_accounts/seeds.yaml \
+  transactions/seeds.yaml
 
