573cdee66c64e3edfb56d91037639e8c8cd3e1f6
Zhao Li <zhao.li@startingspark.com>
2021-03-13 18:27:12 -1000

    seed transactions; update trading account manager's natural key to allow for seed to not use primary keys

--- trading_accounts/managers.py
+++ trading_accounts/managers.py
@@ -1,17 +1,13 @@
-"""Define models"""
+"""Define model managerss"""
 from django.db import models
 from users.models import User
 
 
+# pylint:disable=too-few-public-methods
 class TradingAccountManager(models.Manager):
+    """Define TradingAccount model manager"""
     def get_by_natural_key(self, name, owner_username):
+        """Allow seeds to use natural keys instead of primary keys"""
         owner = User.objects.get(username=owner_username)
         return self.get(name=name, owner=owner)
-
-class TradingAccount(models.Model):
-    """Define TradingAccount model"""
-    objects = TradingAccountManager()
-
-    name = models.CharField(max_length=200)
-    owner = models.ForeignKey('users.user', on_delete=models.CASCADE)
 
