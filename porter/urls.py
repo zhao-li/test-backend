4cb109c22d08f0583e638f503b9663c5813bd0f6
Zhao Li <zhao.li@startingspark.com>
2021-03-21 16:47:24 -1000

    add basic view/action to handle file upload; link import url to project

--- porter/urls.py
+++ porter/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(f'{settings.PATH_PREFIX}', include([
+        path('imports/', include('imports.urls')),
         path('trading-accounts/', include('trading_accounts.urls')),
         path('transactions/', include('transactions.urls')),
         path('greetings/', include('greetings.urls')),
