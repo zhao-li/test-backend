418aad6d3cb48206b15975cfe8d492ebb4b9b4bc
Zhao Li <zhao.li@startingspark.com>
2021-03-21 15:34:00 -1000

    add ApiService, test CSV file, and basic code structure to test uploading a file

--- imports/tests/transactions/test_importing_transactions.py
+++ imports/tests/transactions/test_importing_transactions.py
@@ -2,18 +2,26 @@
 from django.test import TestCase, tag
 from trading_accounts.factories import TradingAccountFactory
 from trading_accounts.models import TradingAccount
+from ..helpers.api_service import ApiService
 
 
 class ImportingTransactionsTest(TestCase):
     """Test Importing Transactions"""
 
     def setUp(self):
-        payload_factory = TradingAccountFactory()
+        self.account = TradingAccountFactory()
+        self.api_service = ApiService()
 
     @tag('integration')
     def test_importing_transactions_to_account(self):
         """test importing transactions to account"""
         initial_number_of_transactions = TradingAccount.objects.count()
+
+        with open('imports/tests/transactions/transactions.csv') as file_pointer:
+            response = self.api_service.post_transactions(
+                self.account.id,
+                file_pointer
+            )
 
         number_of_transactions_created = 1
         expected_number_of_transactions = (
