cbee98eeaf01aa48e6534c16a9dbb458401e86a4
Zhao Li <zhao.li@startingspark.com>
2021-03-28 17:01:17 -1000

    got basic transactions extractor class and test working

--- imports/services/transactions_extractor.py
+++ imports/services/transactions_extractor.py
@@ -1,5 +1,4 @@
 """Define Extractor for Transacations"""
-import csv
 
 
 class TransactionsExtractor():
@@ -9,13 +8,47 @@ class TransactionsExtractor():
     Output: List of transaction strings for each row of transaction data
     """
 
+    START_DELIMITER = '''"Closed Positions"'''
+    STOP_DELIMITER = ""
+
     def __init__(self, raw_data_string):
         self.raw_data_string = raw_data_string
+        self.transaction_lines = []
+        self.started_flag = False
+        self.ended_flag = False
 
     def extract(self):
-        self.reader = csv.reader(self.raw_data_string)
-        for row in self.reader:
-            print("===")
-            print(row)
-        return self.raw_data_string
+        for line in self.raw_data_string.split("\n"):
+            if self._transaction_section_had_started():
+                if self._transaction_section_should_be_stopping(line):
+                    self._stop_on_this_line()
+                else:
+                    self._save(line)
+            else:
+                if self._transaction_section_should_be_starting(line):
+                    self._start_saving_next_lines_as_transactions()
+            if self._this_is_last_line():
+                break 
+        return self.transaction_lines
+
+    def _transaction_section_had_started(self):
+        return self.started_flag == True
+
+    def _transaction_section_should_be_starting(self, line):
+        return line == self.START_DELIMITER
+
+    def _start_saving_next_lines_as_transactions(self):
+        self.started_flag = True
+
+    def _this_is_last_line(self):
+        return self.ended_flag == True
+
+    def _transaction_section_should_be_stopping(self, line):
+        return line == self.STOP_DELIMITER
+
+    def _stop_on_this_line(self):
+        self.ended_flag = True
+
+    def _save(self, line):
+        self.transaction_lines.append(line)
 
--- imports/tests/services/test_transactions_extractor.py
+++ imports/tests/services/test_transactions_extractor.py
@@ -1,18 +1,30 @@
-"""Define tests for transaction parser"""
+"""Define tests for Transactions Extractor"""
 from django.test import TestCase, tag
-from ...services.transaction_parser import TransactionParser
+from ...services.transactions_extractor import TransactionsExtractor
 
 
-class TransactionParserTest(TestCase):
-    """Test Parsing Transactions"""
+class TransactionsExtractorTest(TestCase):
+    """Test Transactions Extractor"""
 
     @tag('unit')
-    def test_parsing_transaction(self):
-        """test parsing transactions"""
+    def test_extracting_transactions(self):
+        """test extracting transactions"""
 
         with open(
             'imports/tests/services/portfolio.csv'
         ) as file_pointer:
             raw_data_string = file_pointer.read()
-            transactions = TransactionParser(raw_data_string).parse()
+            transactions = TransactionsExtractor(raw_data_string).extract()
+
+        first_transaction_index = 0
+        self.assertEqual(
+            transactions[first_transaction_index],
+            '''"","Fastly","FSLY.K","NYSE","01/07/2021","BUY","100.00000000","86.41","01/08/2021","86.41","0.00%","$0.00"'''
+        )
+
+        sescond_transaction_index = 1
+        self.assertEqual(
+            transactions[second_transaction_index],
+            '''"","Overstockcom","OSTK.O","NASDAQ","01/05/2021","BUY","100.00000000","53.42","01/07/2021","57.16","7.00%","$374.00"'''
+        )
 
