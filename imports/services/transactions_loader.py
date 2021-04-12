5bf608adccf28ee39923cf6bfdb6544e98ab5b93
Zhao Li <zhao.li@startingspark.com>
2021-04-11 19:06:23 -1000

    update TransactionsLoader to pass basic loading test

--- imports/services/transactions_loader.py
+++ imports/services/transactions_loader.py
@@ -1,4 +1,8 @@
 """Define Loader for Transactions"""
+from django.core.exceptions import ValidationError
+from django.db.transaction import TransactionManagementError
+from django.db.utils import IntegrityError
+from transactions.models import Transaction
 
 
 class TransactionsLoader():
@@ -7,15 +11,44 @@ class TransactionsLoader():
     Note: load means saving to storage
     Input: 
     Example: 
-    Output: A list of 2 lists
-      * first list is a list of successfully loaded transactions
-      * second list is a list of duplicate transactions that were not loaded
+    Output: A list of 3 lists
+      * first list is a list of transactions that were loaded
+      * second list is a list of transactions that were duplicates
+      * third list is a list of transactions that were not loaded
     Example:
     """
 
-    def __init__(self, transactions):
-        self.transactions = transactions
+    def __init__(self, account, transactions):
+        self.account = account
+        self.queued_transactions = transactions
+        self.loaded_transactions = []
+        self.duplicate_transactions = []
+        self.failed_transactions = []
 
     def load(self):
-        return [True, False]
+        for candidate_transaction in self.queued_transactions:
+            self._process(candidate_transaction)
+
+        return [
+            self.loaded_transactions,
+            self.duplicate_transactions,
+            self.failed_transactions,
+        ]
+
+    def _process(self, transaction):
+        self.staged_transaction = Transaction(
+            account_id=self.account.id,
+            symbol=transaction['symbol']
+        )
+        try:
+            self._check()
+        else:
+            self._load()
+            self.loaded_transactions.append(self.staged_transaction)
+
+    def _check(self):
+        self.staged_transaction.full_clean()
+
+    def _load(self):
+        self.staged_transaction.save()
 
