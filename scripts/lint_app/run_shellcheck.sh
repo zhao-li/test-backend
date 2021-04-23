2761888f2929dfddea6e5c6030b345542a734d4f
Zhao Li <zhao.li@startingspark.com>
2021-04-22 23:09:38 -1000

    fix syntax and add indication that function is meant to be private

--- scripts/lint_app/run_shellcheck.sh
+++ scripts/lint_app/run_shellcheck.sh
@@ -3,14 +3,14 @@
 # This script runs shellcheck to lint shell scripts
 
 findings_found=0
-update_findings_count() {
-  findings_found=$(findings_found + 1)
+_update_findings_count() {
+  findings_found=$((findings_found + 1))
 }
 
-shellcheck --external -- *.sh || update_findings_count
+shellcheck --external -- *.sh || _update_findings_count
 for file in scripts/**/*.sh; do
-  shellcheck --external "$file" || update_findings_count
+  shellcheck --external "$file" || _update_findings_count
 done
 
-exit "$findings_found"
+exit $findings_found
 
