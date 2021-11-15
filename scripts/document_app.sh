1efb01b5a69f442f5cfd59497984987556312850
Zhao Li <zhao.li@startingspark.com>
2021-11-14 17:09:28 -1000

    output documentation to artifacts folder

--- scripts/document_app.sh
+++ scripts/document_app.sh
@@ -2,9 +2,9 @@
 
 # This script documents the application
 
-rm --force --recursive docs/build/* # force is used to suppress warnings when directory is empty
+rm --force --recursive artifacts/docs/* # force is used to suppress warnings when directory is empty
 sphinx-apidoc --force --output-dir docs/source .
-sphinx-build docs/source/ docs/build/
+sphinx-build docs/source/ artifacts/docs/
 #sphinx-build -b coverage docs/source/ docs/build/ # not sure how to get sphinx coverage to work with autodoc
 # https://stackoverflow.com/questions/33436025/sphinx-autodoc-vs-coverage-am-i-missing-something-here
 
