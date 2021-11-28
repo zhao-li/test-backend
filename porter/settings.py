4032d29e64f0f88f88eb1ee55bae305bea057643
Zhao Li <zhao.li@startingspark.com>
2021-11-27 21:57:42 -1000

    verify setting is necessary

--- porter/settings.py
+++ porter/settings.py
@@ -181,7 +181,6 @@ TEST_OUTPUT_FILE_NAME = 'test.xml'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:8081",
-    "http://frontend",
     "http://frontend:8080",
     "http://e2e-frontend:8080",
 ]
