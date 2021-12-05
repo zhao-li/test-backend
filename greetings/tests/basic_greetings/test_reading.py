018d892e32653522c49a6a6f8a70a1a2bf177889
Zhao Li <zhao.li@startingspark.com>
2021-12-05 13:03:18 -1000

    separate reading tests into separate file

--- greetings/tests/basic_greetings/test_reading.py
+++ greetings/tests/basic_greetings/test_reading.py
@@ -1,180 +1,31 @@
-"""Define tests for entire module"""
-import json
+"""Define tests for reading"""
 from django.test import TestCase, tag
 from rest_framework import status
 from ..helpers.api_service import ApiService
-from ..helpers.payload_factory import PayloadFactory
-from ...models import Greeting
-
-TYPE = 'greeting'
-PATH = '/greetings/'
-CONTENT_TYPE = 'application/vnd.api+json'
 
 
-class GreetingsTests(TestCase):
-    """Test CRUD"""
+class ReadingTests(TestCase):
+    """Test reading feature"""
 
     def setUp(self):
         self.api_service = ApiService()
-        self.message = 'An Arbitrary Message'
-        self.payload_factory = PayloadFactory({
-            'message': self.message,
-        })
-
-    @tag('integration')
-    def test_for_json_api_compliancee(self):
-        """test response complies with json api spec"""
-        response = self.client.get(PATH)
-
-        expected_key = 'data'
-        self.assertTrue(expected_key in response.json())
 
     @tag('integration')
     def test_reading(self):
-        """test reading"""
-        response = self.client.get(PATH)
+        """test reading works"""
+        response = self.api_service.get_all()
+        expected_number_of_greetings = 0
+        self.assertEqual(
+            len(response.json()['data']),
+            expected_number_of_greetings
+        )
+
+    @tag('integration')
+    def test_for_json_api_compliance(self):
+        """test response complies with json api spec"""
+        response = self.api_service.get_all()
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    @tag('integration')
-    def test_updating(self):
-        """test updating"""
-
-        original_message = 'Original Greeting'
-        original_json_data = {
-            'data': {
-                'type': TYPE,
-                'attributes': {
-                    'message': original_message
-                }
-            }
-        }
-        response = self.client.post(
-            PATH,
-            json.dumps(original_json_data),
-            content_type=CONTENT_TYPE
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        expected_message = original_message
-        self.assertEqual(
-            Greeting.objects.first().message,
-            expected_message
-        )
-
-        greeting_id = response.json()['data']['id']
-        updated_message = 'Updated Greeting'
-        updated_json_data = response.json()
-        updated_json_data['data']['attributes']['message'] = updated_message
-        response = self.client.patch(
-            PATH + greeting_id + '/',
-            data=json.dumps(updated_json_data),
-            content_type=CONTENT_TYPE,
-        )
-        expected_message = updated_message
-        self.assertEqual(
-            Greeting.objects.get(pk=greeting_id).message,
-            expected_message
-        )
-
-    @tag('integration')
-    def test_deleting(self):
-        """test deleting"""
-
-        no_greetings = 0
-        self.assertEqual(
-            Greeting.objects.count(),
-            no_greetings
-        )
-
-        json_data = {
-            'data': {
-                'type': TYPE,
-                'attributes': {
-                    'message': 'An Arbitrary Greeting'
-                }
-            }
-        }
-
-        response = self.client.post(
-            PATH,
-            json.dumps(json_data),
-            content_type=CONTENT_TYPE,
-        )
-        greeting_id = response.json()['data']['id']
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        one_new_greeting = 1
-        self.assertEqual(
-            Greeting.objects.count(),
-            one_new_greeting
-        )
-
-        response = self.client.delete(
-            PATH + greeting_id + '/',
-            json.dumps(json_data),
-            content_type=CONTENT_TYPE,
-        )
-        self.assertEqual(
-            Greeting.objects.count(),
-            no_greetings
-        )
-
-    @tag('integration')
-    def test_full_lifecycle(self):
-        """test full lifecycle"""
-
-        original_message = 'A Greeting'
-        json_data = {
-            'data': {
-                'type': TYPE,
-                'attributes': {
-                    'message': original_message
-                }
-            }
-        }
-
-        response = self.client.post(
-            PATH,
-            json.dumps(json_data),
-            content_type=CONTENT_TYPE,
-        )
-        greeting_id = response.json()['data']['id']
-        updated_json_data = response.json()
-
-        expected_message = original_message
-        self.assertEqual(
-            Greeting.objects.get(pk=greeting_id).message,
-            expected_message
-        )
-
-        response = self.client.get(PATH)
-        first_greeter = 0
-        self.assertEqual(
-            (response.json()
-                ['data'][first_greeter]
-                ['attributes']['message']
-             ),
-            expected_message
-        )
-
-        updated_message = 'Updated Greeting'
-        updated_json_data['data']['attributes']['message'] = updated_message
-        response = self.client.patch(
-            PATH + greeting_id + '/',
-            data=json.dumps(updated_json_data),
-            content_type=CONTENT_TYPE,
-        )
-        expected_message = updated_message
-        self.assertEqual(
-            Greeting.objects.get(pk=greeting_id).message,
-            expected_message
-        )
-
-        response = self.client.delete(
-            PATH + greeting_id + '/',
-        )
-        no_greetings = 0
-        self.assertEqual(
-            Greeting.objects.count(),
-            no_greetings
-        )
+        expected_key = 'data'
+        self.assertTrue(expected_key in response.json())
 
