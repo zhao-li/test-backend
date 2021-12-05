cbaf58ec24e6bf8709b89682f4aa6de0ddaa42b8
Zhao Li <zhao.li@startingspark.com>
2021-12-05 13:21:55 -1000

    update helpers to work with new features

--- greetings/tests/helpers/api_service.py
+++ greetings/tests/helpers/api_service.py
@@ -12,10 +12,23 @@ class ApiService(Client):
     def __init__(self):
         self.client = Client()
 
+    def get_all(self):
+        return self.client.get(self.PATH)
+
+    def get_one(self, id):
+        return self.client.get(self.PATH + id + '/')
+
     def post(self, payload):
         return self.client.post(
             self.PATH,
             json.dumps(payload),
+            content_type=self.CONTENT_TYPE,
+        )
+
+    def patch(self, id, payload):
+        return self.client.patch(
+            self.PATH + id + '/',
+            data=json.dumps(payload),
             content_type=self.CONTENT_TYPE,
         )
 
--- greetings/tests/helpers/payload_factory.py
+++ greetings/tests/helpers/payload_factory.py
@@ -23,8 +23,25 @@ class PayloadFactory(Client):
             }
         }
 
+    def update_payload(self):
+        return {
+            'data': {
+                'type': self._get_type(),
+                'id': self._get_id(),
+                'attributes': {
+                    'message': self._get_message()
+                }
+            }
+        }
+
     def _get_type(self):
         return self.TYPE
+
+    def _get_id(self):
+        if 'id' in self.overrides:
+            return self.overrides['id']
+        else:
+            return self.DEFAULT_ID
 
     def _get_message(self):
         if 'message' in self.overrides:
