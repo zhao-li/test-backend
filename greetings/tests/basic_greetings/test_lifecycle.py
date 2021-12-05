a1d56f2a7f0fe0896b8b2c642c06dc1013c67b5f
Zhao Li <zhao.li@startingspark.com>
2021-12-05 13:42:59 -1000

    finish up life cycle tests

--- greetings/tests/basic_greetings/test_lifecycle.py
+++ greetings/tests/basic_greetings/test_lifecycle.py
@@ -1,177 +1,54 @@
-"""Define tests for deleting"""
-import json
+"""Define tests for life cycle"""
 from django.test import TestCase, tag
 from rest_framework import status
 from ..helpers.api_service import ApiService
 from ..helpers.payload_factory import PayloadFactory
-from ...models import Greeting
 
 
-class DeletingTests(TestCase):
-    """Test deleting feature"""
-
-    def setUp(self):
-        self.api_service = ApiService()
-        payload_factory = PayloadFactory()
-        response = self.api_service.post(payload_factory.create_payload())
-        self.greeting_id = response.json()['data']['id']
-
-    @tag('integration')
-    def test_deleting(self):
-        """test deleting"""
-
-        initial_number_of_greetings = Greeting.objects.count()
-        self.api_service.delete(self.greeting_id)
-
-        expected_number_of_greetings_deleted = 1
-        expected_number_of_greetings_remaining = (
-            initial_number_of_greetings - expected_number_of_greetings_deleted
-        )
-        self.assertEqual(
-            Greeting.objects.count(),
-            expected_number_of_greetings_remaining
-        )
-        with self.assertRaises(Greeting.DoesNotExist):
-            Greeting.objects.get(pk=self.greeting_id)
-
-    @tag('integration')
-    def test_for_json_api_compliance(self):
-        """test response complies with json api spec"""
-        response = self.api_service.delete(self.greeting_id)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+class LifeCycleTests(TestCase):
+    """Test life cycle"""
 
     def setUp(self):
         self.api_service = ApiService()
+
+    @tag('integration')
+    def test_life_cycle(self):
+        """test life cycle works"""
+
         original_message = 'Arbitrary Message'
         payload_factory = PayloadFactory({
             'message': original_message,
         })
         response = self.api_service.post(payload_factory.create_payload())
-        self.greeting_id = response.json()['data']['id']
-
-    @tag('integration')
-    def test_for_json_api_compliance(self):
-        """test response complies with json api spec"""
-        payload_factory = PayloadFactory({
-            'id': self.greeting_id,
-        })
-        response = self.api_service.patch(
-            self.greeting_id,
-            payload_factory.update_payload()
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        expected_key = 'data'
-        self.assertTrue(expected_key in response.json())
-
-    @tag('integration')
-    def test_for_json_api_compliancee(self):
-        """test response complies with json api spec"""
-        response = self.client.get(PATH)
-
-        expected_key = 'data'
-        self.assertTrue(expected_key in response.json())
-
-    @tag('integration')
-    def test_deleting(self):
-        """test deleting"""
-
-        no_greetings = 0
-        self.assertEqual(
-            Greeting.objects.count(),
-            no_greetings
-        )
-
-        json_data = {
-            'data': {
-                'type': TYPE,
-                'attributes': {
-                    'message': 'An Arbitrary Greeting'
-                }
-            }
-        }
-
-        response = self.client.post(
-            PATH,
-            json.dumps(json_data),
-            content_type=CONTENT_TYPE,
-        )
         greeting_id = response.json()['data']['id']
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        one_new_greeting = 1
-        self.assertEqual(
-            Greeting.objects.count(),
-            one_new_greeting
-        )
-
-        response = self.client.delete(
-            PATH + greeting_id + '/',
-            json.dumps(json_data),
-            content_type=CONTENT_TYPE,
-        )
-        self.assertEqual(
-            Greeting.objects.count(),
-            no_greetings
-        )
-
-    @tag('integration')
-    def test_full_lifecycle(self):
-        """test full lifecycle"""
-
-        original_message = 'A Greeting'
-        json_data = {
-            'data': {
-                'type': TYPE,
-                'attributes': {
-                    'message': original_message
-                }
-            }
-        }
-
-        response = self.client.post(
-            PATH,
-            json.dumps(json_data),
-            content_type=CONTENT_TYPE,
-        )
-        greeting_id = response.json()['data']['id']
-        updated_json_data = response.json()
-
+        response = self.api_service.get_one(greeting_id)
         expected_message = original_message
         self.assertEqual(
-            Greeting.objects.get(pk=greeting_id).message,
+            response.json()['data']['attributes']['message'],
             expected_message
         )
 
-        response = self.client.get(PATH)
-        first_greeter = 0
-        self.assertEqual(
-            (response.json()
-                ['data'][first_greeter]
-                ['attributes']['message']
-             ),
-            expected_message
-        )
+        updated_payload = response.json()
+        updated_message = 'Arbitrary Update'
+        updated_payload['data']['attributes']['message'] = updated_message
+        response = self.api_service.patch(greeting_id, updated_payload)
 
-        updated_message = 'Updated Greeting'
-        updated_json_data['data']['attributes']['message'] = updated_message
-        response = self.client.patch(
-            PATH + greeting_id + '/',
-            data=json.dumps(updated_json_data),
-            content_type=CONTENT_TYPE,
-        )
+        response = self.api_service.get_one(greeting_id)
         expected_message = updated_message
         self.assertEqual(
-            Greeting.objects.get(pk=greeting_id).message,
+            response.json()['data']['attributes']['message'],
             expected_message
         )
 
-        response = self.client.delete(
-            PATH + greeting_id + '/',
-        )
-        no_greetings = 0
+        response = self.api_service.delete(greeting_id)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = self.api_service.get_one(greeting_id)
+        index_of_first_error = 0
+        expected_code = 'not_found'
         self.assertEqual(
-            Greeting.objects.count(),
-            no_greetings
+            response.json()['errors'][index_of_first_error]['code'],
+            expected_code
         )
 
