2aa8804373fabaf83c26054fca270608c4734f1c
Zhao Li <zhao.li@startingspark.com>
2021-12-05 13:57:57 -1000

    link to required issue

--- trading_accounts/tests/basic_trading_accounts/test_reading.py
+++ trading_accounts/tests/basic_trading_accounts/test_reading.py
@@ -16,6 +16,7 @@ class ReadingTests(TestCase):
         response = self.api_service.get_all()
 
         # pylint:disable=duplicate-code ; is not really recognized by linter
+        # update once https://github.com/PyCQA/pylint/issues/214 is closed
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         expected_key = 'data'
         self.assertTrue(expected_key in response.json())
