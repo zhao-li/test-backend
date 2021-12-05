6e9dcc8a8a2b2a6711470f055202df2c13392058
Zhao Li <zhao.li@startingspark.com>
2021-12-05 13:56:25 -1000

    hacky way to disable duplicate-code linter finding

--- trading_accounts/tests/basic_trading_accounts/test_reading.py
+++ trading_accounts/tests/basic_trading_accounts/test_reading.py
@@ -15,6 +15,7 @@ class ReadingTests(TestCase):
         """test response complies with json api spec"""
         response = self.api_service.get_all()
 
+        # pylint:disable=duplicate-code ; is not really recognized by linter
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         expected_key = 'data'
         self.assertTrue(expected_key in response.json())
