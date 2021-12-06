2f4846bdff9febef42fcd1afad2504fba54182ac
Zhao Li <zhao.li@startingspark.com>
2021-12-05 19:54:43 -1000

    update Reading Tests to account for existing Greetings

--- greetings/tests/basic_greetings/test_reading.py
+++ greetings/tests/basic_greetings/test_reading.py
@@ -2,10 +2,11 @@
 from django.test import TestCase, tag
 from rest_framework import status
 from ..helpers.api_service import ApiService
+from ...models import Greeting
 
 
-class ReadingTests(TestCase):
-    """Test reading feature"""
+class ReadingTestsWhenNoGreetings(TestCase):
+    """Test reading feature when no greetings"""
 
     def setUp(self):
         self.api_service = ApiService()
@@ -28,4 +29,38 @@ class ReadingTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         expected_key = 'data'
         self.assertTrue(expected_key in response.json())
+
+class ReadingTestsWhenGreetingsExist(TestCase):
+    """Test reading feature when greetings exist"""
+
+    def setUp(self):
+        self.api_service = ApiService()
+        self.message = 'An Arbitrary Message'
+        Greeting(
+            message=self.message,
+        ).save()
+
+    @tag('integration')
+    def test_reading(self):
+        """test reading works"""
+        response = self.api_service.get_all()
+        expected_number_of_greetings = 1
+        self.assertEqual(
+            len(response.json()['data']),
+            expected_number_of_greetings
+        )
+
+        index_of_first_message = 0
+        received_message = (
+            response.json()
+                ['data']
+                [index_of_first_message]
+                ['attributes']
+                ['message']
+        )
+        expected_message = self.message
+        self.assertEqual(
+            received_message,
+            expected_message
+        )
 
