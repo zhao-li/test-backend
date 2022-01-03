8bbfd37183c74b16dbc53052e37671853bc08be7
Zhao Li <zhao.li@startingspark.com>
2022-01-03 00:40:02 +0000

    Resolve "get test coverage to 100%"

--- scripts/test_app/fill_coverage_gaps.sh
+++ scripts/test_app/fill_coverage_gaps.sh
@@ -3,7 +3,9 @@
 # This script runs through the edge-case scenarios not handled by the normal
 # test suite to get coverage to 100%
 
-pip uninstall --yes django
-coverage run --append ./manage.py check
-pip install --requirement requirements.txt
+SRC_DIR=$(cd "$(dirname "$0")" || exit; pwd -P)
+
+"$SRC_DIR"/fill_coverage_gaps/exercise_django_not_installed.sh
+"$SRC_DIR"/fill_coverage_gaps/exercise_multiple_cors_allowed_origins.sh
+"$SRC_DIR"/fill_coverage_gaps/exercise_seeding.sh
 
